d5a1f040b6fc2d62e4ec59143b91493e542485b7
Tomas Roy <hello@minicreative.net>
2017-08-07 17:41:49 -0400

    connected to Shopify!

--- shopifySync.js
+++ shopifySync.js
@@ -1,35 +1,55 @@
 
 // Initialize libraries
 var nodeSchedule = require('node-schedule');
+var Shopify = require('shopify-api-node');
+var async = require('async');
 var config = require('./config');
-
-// Initialize sync schedule ====================================================
-console.log('shopify-sync started at '+Date());
-console.log(config.production);
-var syncSchedule = nodeSchedule.scheduleJob('10 * * * * *', function () {
-
-	// Run jobs on schedule
-	updateInventory();
-	getOrders();
-	updateShipments();
-
-});
 
 // Jobs ========================================================================
 
+// All Jobs
+var runAllJobs = function () {
+	updateInventory();
+	getOrders();
+	updateShipments();
+};
+
 // Update Inventory: updates Shopify products using the API
 var updateInventory = function () {
-
+	console.log('Updating inventory...');
+	setupShopify();
+	console.log(shopify);
 };
 
 // Get Orders: creates CSV based on incoming orders
 var getOrders = function () {
+	console.log('Getting orders...');
 
 };
 
 // Update Shipments: updates Shopify orders using the API
 var updateShipments = function () {
+	console.log('Updating shipments...');
 
 };
 
 // Functions ===================================================================
+
+var shopify = null;
+var setupShopify = function () {
+	if (!shopify) shopify = new Shopify({
+		shopName: config.shopifyShopName,
+		apiKey: config.shopifyAPIKey,
+		password: config.shopifyPassword
+	});
+};
+
+// Run Program =================================================================
+console.log('shopify-sync started at '+Date());
+runAllJobs();
+
+// Start schedule
+// var syncSchedule = nodeSchedule.scheduleJob(config.schedule, function () {
+// 	console.log('Scheduled tasks started...')
+// 	runAllJobs();
+// });
